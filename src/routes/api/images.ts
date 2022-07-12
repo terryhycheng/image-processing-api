de9a996fa8636c194523d765b798ccaab22c50d2
terryhycheng <terryhycheng@gmail.com>
2022-07-12 15:17:36 +0800

    Returning cache image function added

--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -2,28 +2,39 @@ import express from 'express';
 import sharp from 'sharp';
 import { promises as fs } from 'fs';
 
-const images = express.Router();
+const images: express.Router = express.Router();
+const full_files: string[] = [];
+const thumb_files: string[] = [];
 
-images.get('/', async (req, res) => {
-  //Loop over the dir and create an array
-  const dir = await fs.opendir(`./images/full`);
-  const files: string[] = [];
-  for await (const dirent of dir) {
-    files.push(dirent.name);
+images.use('/', async (req, res, next): Promise<void> => {
+  //Loop over the 'images/full' dir and create an array including all file names.
+  const full_dir = await fs.opendir(`./images/full`);
+  for await (const dirent of full_dir) {
+    full_files.push(dirent.name);
   }
-  //Check if query contains a file name
-  if (files.includes(`${req.query.filename}.jpg`)) {
+  //Loop over the 'images/thumb' dir and create an array including all file names.
+  const thumb_dir = await fs.opendir(`./images/thumb`);
+  for await (const dirent of thumb_dir) {
+    thumb_files.push(dirent.name);
+  }
+  next();
+});
+
+images.get('/', async (req, res): Promise<void> => {
+  //Check if query contains a file name, return an error message if false
+  if (full_files.includes(`${req.query.filename}.jpg`)) {
     const { filename, width, height } = req.query;
     const width_number: number = parseInt(width as string);
     const height_number: number = parseInt(height as string);
-    const file: sharp.Sharp = sharp(`./images/full/${filename}.jpg`);
+    const filename_string: string = filename as unknown as string;
+    const file: sharp.Sharp = sharp(`./images/full/${filename_string}.jpg`);
     try {
       res.writeHead(200, { 'Content-Type': 'image/jpeg' });
       const data = await toBuffer(
         width_number,
         height_number,
         file,
-        filename as string
+        filename_string
       );
       res.end(data);
     } catch (error) {
@@ -42,12 +53,21 @@ const toBuffer = async (
   file: sharp.Sharp,
   filename: string
 ): Promise<Buffer> => {
+  //Check if width & height values are not valid, if true, return an original image
   if (!width || !height || width <= 0 || height <= 0) {
     return file.jpeg().toBuffer();
   } else {
-    const data = file.resize(width, height).jpeg().toBuffer();
-    toOutput(await data, filename, width, height);
-    return data;
+    //If false, check if there is any cache with similar file, return that file if it's true.
+    if (thumb_files.includes(`${filename}_${width}x${height}.jpg`)) {
+      return await fs.readFile(
+        `./images/thumb/${filename}_${width}x${height}.jpg`
+      );
+      //Otherwise, create a new one.
+    } else {
+      const data = file.resize(width, height).jpeg().toBuffer();
+      toOutput(await data, filename, width, height);
+      return data;
+    }
   }
 };
 
